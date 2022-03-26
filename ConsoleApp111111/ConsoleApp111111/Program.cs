8a4f390d6cd1787b84f5ad057c8b8bdcd0d4d9ce
Sackaram <vicken_92@hotmail.com>
2022-03-26 20:13:23 +0100

    Testarwtf
    uuuh...

---
+++ ConsoleApp111111/ConsoleApp111111/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp111111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
